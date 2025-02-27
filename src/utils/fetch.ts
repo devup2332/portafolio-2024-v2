3a99670000145b4e4b837def04fb34dce4fbb384
Diego Rojas <devup2332@gmail.com>
2025-02-27 11:54:40 -0500

    master: Edited top margin in banner section

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -3,7 +3,7 @@ import { env } from "./env";
 export const fetchAPI = async (path: string, config: RequestInit) => {
   const url = `${env.API_URL}${path.startsWith("/") ? path : `/${path}`}`;
   const res = await fetch(url, config);
-  if (!res.ok) {
+  if (res.status !== 200) {
     throw new Error(res.statusText);
   }
   return await res.json();
